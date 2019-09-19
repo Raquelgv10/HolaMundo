fd432196f0b5ce61c132a21ca554c25936bccaad
a18raquelgv <a18raquelgv@192.168.56.1>
2019-09-19 10:36:43 +0200

    Con clase Coche.

---
+++ HolaMundo/src/Coche.java
@@ -0,0 +1,11 @@
+
+public class Coche {
+
+	public Coche() {
+		System.out.println("Son un coche rápido");
+		// TODO Apéndice de constructor generado automáticamente
+	}
+
+
+
+}
--- HolaMundo/src/HolaM.java
+++ HolaMundo/src/HolaM.java
@@ -3,6 +3,7 @@ public class HolaM {
 	public static void main(String[] args) {
 		
           System.out.println("Hola Mundo");
+          new Coche();
           
           
 	}
